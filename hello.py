bf61128b0b205eba672e3615b256f48ebdc5a0d5
Ashish Prajapati <108178021+codingashishdev@users.noreply.github.com>
2026-05-03 20:36:09 +0530

    Add echo and message handling endpoints to the API

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 from flask import Flask, url_for
 from flask import request
+from flask import json
 
 app = Flask(__name__)
 
@@ -21,6 +22,43 @@ def api_hello():
         return "Hello" + request.args['name']
     else:
         return "Hello John Doe"
+ 
+
+@app.route("/echo", methods = ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'])
+def api_echo():
+    if request.method == 'GET':
+        return "echo: GET\n"
+    
+    elif request.method == 'POST':
+        return "echo: POST\n"
+
+    elif request.method == 'PATCH':
+        return "echo: PATCH\n"
+    
+    elif request.method == 'PUT':
+        return "echo: PUT\n"
+    
+    elif request.method == 'DELETE':
+        return "echo: DELETE\n"
+ 
+
+@app.route('/messages', methods = ['POST'])
+def api_message():
+    if request.headers['Content-Type'] == 'text/plain':
+        return "Text message: " + request.data  
+
+    elif request.headers['Content-Type'] == 'application/json':
+        return "JSON message: " + json.dumps(request.data)
+    
+    elif request.headers['Content-Type'] == 'application/octet-stream':
+        f = open('./binary', 'wb')
+        f.write(request.data)
+        f.close()
+        return "Binary message written!"
+    
+    else:
+        return "415 Unsupported Media Type ;)"
+  
  
 if __name__ == "__main__":
     app.run()
